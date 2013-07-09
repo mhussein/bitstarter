e7a75b913ef18cfd5410a910af77e0e0aed0b23d
Mohamed Hussein <mohamed_hussein@mentor.com>
2013-07-09 16:13:44 +0200

    Updated grader to support urls. Added all needed checks for hw3.

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +63,36 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var performCheck = function(file, checks) {
+	console.log("Checking %s", file);
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'url to site')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		
+	if (program.url != null) {
+				file = "temp.html";
+		rest.get(program.url).on('complete', function(result) {
+			if (result instanceof Error) {
+				sys.puts('Error: ' + result.message);
+				this.retry(5000); // try again after 5 sec
+			} else {
+				fs.writeFile(__dirname + '/temp.html', result, function(err) {
+					if (err) throw err;
+					console.log('Temp file saved!');
+					performCheck("temp.html", program.checks);
+				});
+			}
+		});
+	} else { 
+		performCheck(program.file, program.checks);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
